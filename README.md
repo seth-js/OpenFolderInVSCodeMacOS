3a7539c59aef227b0f5010c26b38063996c645a5
seth-js <83692925+seth-js@users.noreply.github.com>
2023-01-13 16:30:50 -0600

    Added files

---
+++ README.md
@@ -0,0 +1,11 @@
+# OpenFolderInVsCodeMacOS
+
+Plugin for [fman.io](https://fman.io) to open Visual Studio Code for the current folder or individual selected files.
+
+### Usage
+
+Press **F3** to open the current directory or selected files in Visual Studio Code.
+
+### Notice
+
+This is simply a very minor rewrite of [Joaquín Bernal](https://github.com/jobedom)'s [plugin](https://github.com/jobedom/OpenFolderInVSCode) made to work with macOS.
---
+++ open_folder_in_vscode/__init__.py
@@ -0,0 +1,29 @@
+from fman import DirectoryPaneCommand
+from fman.url import as_human_readable
+import subprocess
+import os.path
+
+
+class OpenFolderInVSCode(DirectoryPaneCommand):
+    default_exe_path = 'code'
+    unix_exe_path = '/usr/local/bin/code'
+
+    def __call__(self):
+        paths = self.get_file_paths()
+        exe_path = self.get_exe_path()
+
+        args = [exe_path]
+        args.extend(paths)
+        subprocess.run(args)
+
+    def get_file_paths(self):
+        selection = self.pane.get_selected_files()
+        if selection:
+            return [as_human_readable(p) for p in selection]
+        return [as_human_readable(self.pane.get_path())]
+
+    def get_exe_path(self):
+        # Use (default?) unix path if it exists
+        if os.path.isfile(self.unix_exe_path):
+            return self.unix_exe_path
+        return self.default_exe_path
